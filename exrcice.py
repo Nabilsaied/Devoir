bc67a1e9821630f274e6382eba670dba7e317bf7
Nabil <nabil.saied@ecoles-epsi.net>
2024-01-31 12:30:51 +0100

    programme

--- exrcice.py
+++ exrcice.py
@@ -2,11 +2,20 @@
 print("Multiplication ")
 
 nb=int(input("Donner votre nombre : "))
-print("Vous avez choisir",nb)
+print("Vous avez choisir :",nb)
 
 
 
 for i in range(11):
     print(nb * i)
 
+choix = input("Veux-tu recommencer? (Oui ou Non) : ").lower()
 
+if choix == "oui":
+    nb = int(input("Donner un autre  nombre : "))
+    for i in range(11):
+        print(nb * i)
+elif choix == "non":
+    print("C'est fini ")
+else:
+    print("Choix non valide. On recommence.")
