bbdf7b83d30fe01035c20cbea76f46bcef87bb8a
Nabil <nabil.saied@ecoles-epsi.net>
2024-01-31 12:25:55 +0100

    saisir un nombre, et affiche ce nombre

--- exrcice.py
+++ exrcice.py
@@ -1,11 +1,9 @@
-print("Menu:")
-print("1. Ajouter un nombre ")
-print("2. Afficher les multiples de 1 à 10 ")
-print("3. Vous souhaitez choisir un autre nombre ")
-print("4. Quitter")
 
-choix=int(input("Choissir un nombre pour acceder au menu :"))
-if choix==1:
-    nb=int(input("Donner votre nombre : "))
+print("Multiplication ")
+
+nb=int(input("Donner votre nombre : "))
 print("Vous avez choisir",nb)
+
+
+
 
